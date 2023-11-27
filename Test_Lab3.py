9db0df11b9772934bbcf05a133193e4f4282870f
how <eonghowfoh@gmail.com>
2023-11-27 20:23:07 +0800

    idk

--- Test_Lab3.py
+++ Test_Lab3.py
@@ -5,10 +5,10 @@ print("Test_Lab3")
 
 def test_bubble_sort_ascending():
     result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
+    arr = [64, 34, 25, 12, 22, 11, 90]
     test_arr = [11, 12, 22, 25, 34, 64, 90]
 
-    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+    result = Lab3.bubble_sort(arr, Lab3.SORT_ASCENDING)
 
     assert (result == test_arr)
 
@@ -28,3 +28,23 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+def test_large_number():
+    result = []
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 5, 6, 7]
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == 1)
+
+def test_no_number():
+    result = []
+    input_arr = []
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+    assert (result == 0)
+
+def test_alphabets():
+    result = []
+    input_arr = [64, 333, 6, "abh", 33, 22, 3, 8]
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+    assert (result == 2)
